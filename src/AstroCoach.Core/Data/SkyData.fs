7a35bea6fc27e97f69ec3b547606cc8fa86d35ae
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2026-01-01 16:49:10 +0100

    Create base data types for sky objects

---
+++ src/AstroCoach.Core/Data/SkyData.fs
@@ -0,0 +1,23 @@
+﻿module AstroCoach.Core.SkyData
+
+type BaseSkyObject = {
+    id: int64
+    ra: float
+    dec: float
+    time: System.DateTime
+}
+
+type StarInfo = {
+    magnitude: float
+    spectralType: string
+}
+
+type SkyObject =
+| Star of BaseSkyObject * StarInfo
+
+type ConstellationInfo = {
+    id: int64
+    shortName: string
+    fullName: string
+    stars: SkyObject list
+}
